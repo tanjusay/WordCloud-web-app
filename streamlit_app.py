808a0ff7d84fb410ae14704f33bf4cfa63dbb533
tanjusay <112522489+tanjusay@users.noreply.github.com>
2023-06-06 21:33:13 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,42 +1,53 @@
 import streamlit as st
-from wordcloud import WordCloud
-from io import BytesIO
 import PyPDF2
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from io import BytesIO
 
 
-def extract_text_from_pdf(file):
-    pdf_reader = PyPDF2.PdfFileReader(file)
+def extract_important_words(pdf_file):
+    # Extract text from PDF
+    pdf_reader = PyPDF2.PdfFileReader(pdf_file)
     text = ""
-    for page in range(pdf_reader.getNumPages()):
+    for page in range(pdf_reader.numPages):
         text += pdf_reader.getPage(page).extractText()
-    return text
+
+    # Perform text processing and extract important words
+    # Replace with your own logic for extracting important words
+
+    # Example: Extract words longer than 4 characters
+    important_words = [word for word in text.split() if len(word) > 4]
+
+    return important_words
 
 
+def generate_wordcloud(important_words):
+    # Generate word cloud from important words
+    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(' '.join(important_words))
 
-def generate_wordcloud(text):
-    wordcloud = WordCloud().generate(text)
-    return wordcloud
+    # Plot word cloud
+    plt.figure(figsize=(10, 5))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
 
+    # Convert plot to image
+    image_stream = BytesIO()
+    plt.savefig(image_stream, format='png')
+    image_stream.seek(0)
 
-def main():
-    st.title("Word Cloud Generator")
-    st.write("Upload a PDF, or TXT file to generate a word cloud.")
-
-    file = st.file_uploader("Upload File", type=["pdf", "txt"])
-
-    if file is not None:
-        file_type = file.name.split(".")[-1]
-
-        if file_type == "pdf":
-            text = extract_text_from_pdf(file)
-        elif file_type == "txt":
-            text = file.read().decode("utf-8")
-
-        wordcloud = generate_wordcloud(text)
-
-        st.subheader("Word Cloud")
-        st.image(wordcloud.to_image())
+    return image_stream
 
 
-if __name__ == "__main__":
-    main()
+# Main application
+st.title("PDF Word Cloud Generator")
+uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
+
+if uploaded_file is not None:
+    # Extract important words from PDF
+    important_words = extract_important_words(uploaded_file)
+
+    # Generate word cloud image
+    wordcloud_image = generate_wordcloud(important_words)
+
+    # Display word cloud image
+    st.image(wordcloud_image, caption="Word Cloud", use_column_width=True)
